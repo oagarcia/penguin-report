b0a1d641074f4da5d3792425314b4598da2bdb9d
Andrés Zorro <andres.zorro@zemoga.com>
2016-02-21 23:05:59 -0500

    Express refactor

--- source/index.js
+++ source/index.js
@@ -191,7 +191,7 @@ app.get('/sync-user', function (req, res) {
   }
 });
 
-// Catch not found
+// Catch all not found
 app.use('*', (req, res) => res.status(404).send(`<h1>404 Not Found</h1>`));
 
 app.listen(process.env.PORT || 80, () =>
